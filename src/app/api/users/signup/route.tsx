676addc251f5ed1eeae1f1bb20628d534ce360b6
Lenny Thobejane <Lennythobejane@gmail.com>
2024-09-18 21:02:47 +0200

    navbar created with the right links

--- src/app/api/users/signup/route.tsx
+++ src/app/api/users/signup/route.tsx
@@ -1,7 +1,1 @@
-// first thing we have to do is connect to the database
-import { connect } from "@/database/config.ts";
 
-// import the user model
-import User from "@/models/UserModel";
-
-connect();
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,6 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import { axios } from "axios";
 
 // MUI
 import Stack from "@mui/material/Stack";
@@ -12,50 +11,49 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 
 const LoginPage = () => {
-	const [user, setUser] = useState({ email: "", password: "" });
+    const [user, setUser] = useState({ email: "", password: "" });
 
-	const handleLogin = () => {};
-	return (
-		<Stack height={"100vh"} justifyContent={"center"} alignItems={"center"}>
-			<Stack width={"400px"} spacing={2}>
-				<Typography variant={"h4"} sx={{ textAlign: "center" }}>
-					Login
-				</Typography>
-				<TextField
-					label="Email"
-					type="email"
-					variant="outlined"
-					value={user.email}
-					onChange={(e) =>
-						setUser({ ...user, email: e.target.value })
-					}
-					sx={{ backgroundColor: "white", borderRadius: "5px" }}
-				/>
-				<TextField
-					label="password"
-					type="password"
-					variant="outlined"
-					value={user.password}
-					onChange={(e) =>
-						setUser({ ...user, password: e.target.value })
-					}
-					sx={{ backgroundColor: "white", borderRadius: "5px" }}
-				/>
-				<Button
-					variant="outlined"
-					color="primary"
-					onClick={handleLogin}
-					sx={{ borderColor: "white", color: "white" }}
-				>
-					Login
-				</Button>
+    const handleLogin = () => {
+        // API call to check and validate the user
+        console.log("User", user);
+    };
+    return (
+        <Stack height={"100vh"} justifyContent={"center"} alignItems={"center"}>
+            <Stack width={"400px"} spacing={2}>
+                <Typography variant={"h4"} sx={{ textAlign: "center" }}>
+                    Login
+                </Typography>
+                <TextField
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    value={user.email}
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                    sx={{ backgroundColor: "white", borderRadius: "5px" }}
+                />
+                <TextField
+                    label="password"
+                    type="password"
+                    variant="outlined"
+                    value={user.password}
+                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                    sx={{ backgroundColor: "white", borderRadius: "5px" }}
+                />
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={handleLogin}
+                    sx={{ borderColor: "white", color: "white" }}
+                >
+                    Login
+                </Button>
 
-				<Stack alignItems={"center"}>
-					<Link href="/signup">Navigate to Signup</Link>
-				</Stack>
-			</Stack>
-		</Stack>
-	);
+                <Stack alignItems={"center"}>
+                    <Link href="/signup">Navigate to Signup</Link>
+                </Stack>
+            </Stack>
+        </Stack>
+    );
 };
 
 export default LoginPage;
