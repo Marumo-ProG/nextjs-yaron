1c26d0fc1f7b6b797daaa5b2c7919645ea848f2e
Lenny Thobejane <Lennythobejane@gmail.com>
2024-09-19 08:46:58 +0200

    login functionality

---
+++ src/app/api/users/login/route.tsx
@@ -0,0 +1,6 @@
+import { Client } from "appwrite";
+
+// initialize the client
+const client = new Client();
+
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("66eb25660024e25e0710");
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,14 @@
 // this is the login page component that will be rendered when the user navigates to the /login route
-
 "use client";
+// Appwrite
+import { Client, Databases } from "appwrite";
+
+// initialize the client
+const client = new Client();
+
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("66eb25660024e25e0710");
+
+const databases = new Databases(client);
 import React, { useState } from "react";
 import Link from "next/link";
 
@@ -9,13 +17,26 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import { CircularProgress } from "@mui/material";
 
 const LoginPage = () => {
     const [user, setUser] = useState({ email: "", password: "" });
+    const [loader, setLoader] = useState(false);
 
-    const handleLogin = () => {
+    const handleLogin = async () => {
+        setLoader(true);
         // API call to check and validate the user
-        console.log("User", user);
+        const { documents } = await databases.listDocuments(
+            "66eb2685003012cd34d1", // databaseId
+            "66eb26a8001e730d10b6", // collectionId
+            [] // queries (optional)
+        );
+        if (documents[0].email === user.email && documents[0].password === user.password) {
+            alert("Login successful");
+        } else {
+            alert("Login failed");
+        }
+        setLoader(false);
     };
     return (
         <Stack height={"100vh"} justifyContent={"center"} alignItems={"center"}>
@@ -42,6 +63,7 @@ const LoginPage = () => {
                 <Button
                     variant="outlined"
                     color="primary"
+                    endIcon={loader && <CircularProgress size={20} />}
                     onClick={handleLogin}
                     sx={{ borderColor: "white", color: "white" }}
                 >
